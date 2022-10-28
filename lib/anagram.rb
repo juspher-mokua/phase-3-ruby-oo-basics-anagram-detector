b46bbbeed161c65e8514f8757ef3a5f304e0752e
Juspher Mokua <juspermokua6@gmail.com>
2022-10-28 23:43:38 +0300

    done assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class  Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+    def match(list_word)
+        list_word.select do |wo|
+            wo.downcase.chars.sort.join == @word.downcase.chars.sort.join
+        end
+    end
+end 
